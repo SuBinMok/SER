1c9778723116928aedabd5a5b2a1fcd3e0fb20aa
mok subin <0313043@gmail.com>
2021-02-01 15:44:38 +0900

    play .WAV

---
+++ play_audio.py
@@ -0,0 +1,32 @@
+"""
+
+import pyaudio
+import wave
+
+chunk = 1024
+
+path = 'C://Users//03130//Desktop//2_석사//debate_audios//debate_audios//p1p2.wav'
+
+with wave.open(path, 'rb') as f:
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
+                    channels=f.getnchannels(),
+                    rate=f.getframerate(),
+                    output=True)
+
+    data = f.readframes(chunk)
+    while data:
+        stream.write(data)
+        data = f.readframes(chunk)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+"""
+#-*-coding : utf-8-*-
+
+from playsound import playsound
+
+
+playsound('C://Users//03130//Desktop//2_master//debate_audios//debate_audios//p3p4.wav', True)
