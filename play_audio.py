78677ad6902113275f0e1af5f5c711023a1f37c4
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2021-02-08 17:51:24 +0900

    play audio

--- play_audio.py
+++ play_audio.py
@@ -1,32 +1,6 @@
-"""
-
-import pyaudio
-import wave
-
-chunk = 1024
-
-path = 'path'
-
-with wave.open(path, 'rb') as f:
-    p = pyaudio.PyAudio()
-    stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
-                    channels=f.getnchannels(),
-                    rate=f.getframerate(),
-                    output=True)
-
-    data = f.readframes(chunk)
-    while data:
-        stream.write(data)
-        data = f.readframes(chunk)
-
-    stream.stop_stream()
-    stream.close()
-
-    p.terminate()
-"""
 #-*-coding : utf-8-*-
 
 from playsound import playsound
 
-
-playsound('path', True)
+dir = "path"
+playsound(dir, True)
